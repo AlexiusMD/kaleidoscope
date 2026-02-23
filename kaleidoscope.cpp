016c1521d77a2b93731e807cfc29558d289d5d19
Alexius Maliuk Dias <alexius.m@edu.pucrs.br>
2026-02-23 15:53:47 -0300

    feat: main driver loop

--- kaleidoscope.cpp
+++ kaleidoscope.cpp
@@ -414,6 +414,10 @@ static void MainLoop() {
     }
 }
 
+//===----------------------------------------------------------------------===//
+// Main driver code.
+//===----------------------------------------------------------------------===//
+
 int main() {
     // Install standard binary operators.
     // 1 is lowest precedence.
@@ -421,4 +425,13 @@ int main() {
     BinopPrecedence['+'] = 20;
     BinopPrecedence['-'] = 20;
     BinopPrecedence['*'] = 40; // highest
+
+    // Prime the first token.
+    fprintf(stderr, "ready> ");
+    getNextToken();
+
+    // Run the main "interpreter loop" now.
+    MainLoop();
+
+    return 0;
 }
